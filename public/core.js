184f7b11e6ddb3471b64a35100e6f038d17e1034
Allyson <allysonmichellewhite@gmail.com>
2015-12-07 14:02:07 -0400

    task complete but not working

--- public/core.js
+++ public/core.js
@@ -1,2 +1,41 @@
 // core.js
 // @DESCRIPTION: this file will hold the angular code for your app
+var scotchTodo = angular.module('scotchTodo', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	//when landing on this page get all todos and display them
+	$http.get('api/todos')
+		.success(function(data) {
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+
+		//when submitting the add form send the text t the node api
+		$scope.createTodo = function() {
+			$http.post('/api/todos', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {} //clear the form
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+		};
+		//delete a todo after checking it
+		$scope.deleteTodo = function(id) {
+			$http.delete('/api/todos/' + id)
+			.success(function(data) {
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+		};
+}
